83356a881825a605bacc4ac64d0967d2be7a6646
JReyCodes <121682721+JReyCodes@users.noreply.github.com>
2024-01-12 13:46:40 -0500

    added some test changes

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const App = () => {
     return (
-        <h1>Justin's Site</h1>
+        <div>
+            <h1>Justin's Site</h1>
+            <h2>Something about me</h2>
+            <p>Charlie really my biggest fan</p>
+        </div>
     )
 }
 
--- client/index.js
+++ client/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-
-//import { Provider } from 'react-redux';
-// import store from './redux/store.js'
-
+import './style.css'
 
 const root = createRoot(document.getElementById('root'));
 
