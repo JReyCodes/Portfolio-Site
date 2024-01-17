e8337279f0b69feb250973eb6de333d329ded84c
JReyCodes <121682721+JReyCodes@users.noreply.github.com>
2024-01-16 23:56:29 -0500

    added some basic info to about page

--- client/About.jsx
+++ client/About.jsx
@@ -4,6 +4,8 @@ const About = () => {
     return (
         <div id="about-div">
             <img id="me-img" src="https://i.ibb.co/S3nrydD/IMG-5780.jpg"></img>
+            <h1>About Me</h1>
+            <p>Before coding I was a professional Call Of Duty Player i traveled around the us playing in different tournaments.</p>
         </div>
         )
 }
